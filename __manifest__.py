8ba582d273c463179170de9138413b95bc4cb615
Marcos-MwM <marcos.eduar143@gmail.com>
2026-06-05 20:15:47 -0500

    modelo completo

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{ 'name':'Tienda de videojuegos',
+    'version':'1.0',
+    'summary':' Tienda de videojuegos',
+    'autor':'Marcos Mena',
+    'data':{'views/tienda_juegos_views.xml',
+            'security/ir.model.access.csv'},
+    'depends':['base'],
+    'category': 'Sales',
+    'license': 'LGPL-3',
+    
+'application':True,
+'installable':True}
